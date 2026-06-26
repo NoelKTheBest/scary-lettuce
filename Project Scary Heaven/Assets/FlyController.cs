bfe1c9131232876b76bebb53681b06fc3736c2a0
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-06-25 17:19:40 -0700

    Add project and setup initial flight mechanic

---
+++ Project Scary Heaven/Assets/FlyController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class FlyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float maxFloatHeight = 10;
+    public float minFloatHeight;
+
+    public Camera freeLookCamera;
+    private float currentHeight;
+    private Animator anim;
+    private float xRotation;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHeight = transform.position.y;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRotation = freeLookCamera.transform.rotation.eulerAngles.x;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveCharacter();
+        }
+        else
+        {
+            DisableMovement();
+        }
+
+        currentHeight = Mathf.Clamp(transform.position.y, currentHeight, maxFloatHeight);
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 cameraForward = new Vector3(freeLookCamera.transform.forward.x, 0, freeLookCamera.transform.forward.z);
+        transform.rotation = Quaternion.LookRotation(cameraForward);
+        transform.Rotate(new Vector3(xRotation, 0, 0), Space.Self);
+
+        anim.SetBool("isFlying", true);
+
+        Vector3 forward = freeLookCamera.transform.forward;
+        Vector3 flyDirection = forward.normalized;
+
+        currentHeight += flyDirection.y * moveSpeed * Time.deltaTime;
+        currentHeight = Mathf.Clamp(currentHeight, minFloatHeight, maxFloatHeight);
+
+        transform.position += flyDirection * moveSpeed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+    }
+
+    private void DisableMovement()
+    {
+        anim.SetBool("isFlying", false);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
+}
